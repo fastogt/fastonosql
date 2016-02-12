2b1448ae773b567a1c532b90a98a17f2ff9818da
topilski <atopilski@gmail.com>
2016-02-12 09:25:05 +0300

    Fix logging path

--- src/server_config_daemon/server_config.h
+++ src/server_config_daemon/server_config.h
@@ -3,5 +3,5 @@
 #define GET_FASTONOSQL_VERSION "FASTONOSQL_VERSION"
 #define GET_FASTOREDIS_VERSION "FASTOREDIS_VERSION"
 
-#define FASTONOSQL_URL "46.101.8.183"
-#define FASTOREDIS_URL "46.101.8.183"
+#define FASTONOSQL_URL "siteonyourdevice.com"
+#define FASTOREDIS_URL "siteonyourdevice.com"
