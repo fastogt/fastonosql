6357435c61aef225f359e3e47bfedfbe7c3ca8c5
topilski <atopilski@gmail.com>
2016-02-24 07:07:32 +0300

    Typo

--- src/core/rocksdb/rocksdb_raw.cpp
+++ src/core/rocksdb/rocksdb_raw.cpp
@@ -2,18 +2,18 @@
 
     This file is part of FastoNoSQL.
 
-    SiteOnYourDevice is free software: you can redistribute it and/or modify
+    FastoNoSQL is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    SiteOnYourDevice is distributed in the hope that it will be useful,
+    FastoNoSQL is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with SiteOnYourDevice.  If not, see <http://www.gnu.org/licenses/>.
+    along with FastoNoSQL.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "core/rocksdb/rocksdb_raw.h"
