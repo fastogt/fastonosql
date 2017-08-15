98c42fafc5ac1b2238125d94844600cc2657d887
topilski <atopilski@gmail.com>
2017-08-15 08:29:17 +0300

    Add needed files

---
+++ src/gui/utils.cpp
@@ -0,0 +1,60 @@
+/*  Copyright (C) 2014-2017 FastoGT. All right reserved.
+
+    This file is part of FastoNoSQL.
+
+    FastoNoSQL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FastoNoSQL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FastoNoSQL.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "gui/utils.h"
+
+#include <QFileDialog>
+
+namespace fastonosql {
+namespace gui {
+
+QString ShowSaveFileDialog(QWidget* parent, const QString& title, const QString& directory, const QString& filter) {
+#if defined(OS_WIN) || defined(OS_MACOSX)
+  return QFileDialog::getSaveFileName(parent, title, directory, filter);
+#else
+  QFileDialog dialog(parent, title, directory, filter);
+  if (parent) {
+    dialog.setWindowModality(Qt::WindowModal);
+  }
+  QRegExp filter_regex(QLatin1String("(?:^\\*\\.(?!.*\\()|\\(\\*\\.)(\\w+)"));
+  QStringList filters = filter.split(QLatin1String(";;"));
+  if (!filters.isEmpty()) {
+    dialog.setNameFilter(filters.first());
+    if (filter_regex.indexIn(filters.first()) != -1) {
+      dialog.setDefaultSuffix(filter_regex.cap(1));
+    }
+  }
+  dialog.setAcceptMode(QFileDialog::AcceptSave);
+  if (dialog.exec() == QDialog::Accepted) {
+    QString file_name = dialog.selectedFiles().first();
+    QFileInfo info(file_name);
+    if (info.suffix().isEmpty() && !dialog.selectedNameFilter().isEmpty()) {
+      if (filter_regex.indexIn(dialog.selectedNameFilter()) != -1) {
+        QString extension = filter_regex.cap(1);
+        file_name += QLatin1String(".") + extension;
+      }
+    }
+    return file_name;
+  } else {
+    return QString();
+  }
+#endif
+}
+
+}  // namespace gui
+}  // namespace fastonosql
