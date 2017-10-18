a661104a03f17b98c72aec16b9b5b2a644817b20
topilski <atopilski@gmail.com>
2017-10-18 11:37:21 +0300

    Build env review

--- build/build_env.py
+++ build/build_env.py
@@ -13,13 +13,13 @@ ARCH_OPENSSL_EXT = "tar." + ARCH_OPENSSL_COMP
 
 g_script_path = os.path.realpath(sys.argv[0])
 
+
 def print_usage():
     print("Usage:\n"
           "[optional] argv[1] platform\n"
           "[optional] argv[2] architecture\n"
           "[optional] argv[3] build system for common/qscintilla/libssh2 (\"ninja\", \"make\", \"gmake\")\n"
-          "[optional] argv[4] build system for leveldb/rocksdb/upscaledb (\"make\", \"gmake\")\n"
-          "[optional] argv[5] prefix path\n")
+          "[optional] argv[4] prefix path\n")
 
 
 def print_message(progress, message):
@@ -28,77 +28,48 @@ def print_message(progress, message):
 
 
 class BuildRequest(object):
-    def __init__(self, platform, arch_bit):
+    def __init__(self, platform, arch_bit, dir_path):
         platform_or_none = system_info.get_supported_platform_by_name(platform)
 
         if not platform_or_none:
             raise utils.BuildError('invalid platform')
 
         arch = platform_or_none.architecture_by_arch_name(arch_bit)
-        if arch == None:
+        if not arch:
             raise utils.BuildError('invalid arch')
+
+        build_dir_path = os.path.abspath(dir_path)
+        if os.path.exists(build_dir_path):
+            shutil.rmtree(build_dir_path)
+
+        os.mkdir(build_dir_path)
+        os.chdir(build_dir_path)
+
+        self.build_dir_path_ = build_dir_path
 
         self.platform_ = platform_or_none.make_platform_by_arch(arch, platform_or_none.package_types())
         print("Build request for platform: {0}, arch: {1} created".format(platform, arch.name()))
 
-    def build(self, dir_path, bs, bs_external, prefix_path):
-        cmake_project_root_abs_path = '..'
-        if not os.path.exists(cmake_project_root_abs_path):
-            raise utils.BuildError('invalid cmake_project_root_path: %s' % cmake_project_root_abs_path)
-
-        if not bs:
-            bs = system_info.SUPPORTED_BUILD_SYSTEMS[0]
-
-        if not bs_external:
-            bs_external = system_info.SUPPORTED_BUILD_SYSTEMS[1]
-
-        if prefix_path == None:
-            prefix_path = self.platform_.arch().default_install_prefix_path()
-
-        abs_dir_path = os.path.abspath(dir_path)
-        if os.path.exists(abs_dir_path):
-            shutil.rmtree(abs_dir_path)
-
-        is_android = self.platform_.name() == 'android'
-
-        generator = bs.cmake_generator_arg()
-        build_system_args = bs.cmd_line()
-        # bs_name = bs.name()
-
-        pwd = os.getcwd()
-        os.mkdir(abs_dir_path)
-        os.chdir(abs_dir_path)
-
-        # project static options
-        prefix_args = '-DCMAKE_INSTALL_PREFIX={0}'.format(prefix_path)
-
-        cmake_line = ['cmake', cmake_project_root_abs_path, generator, '-DCMAKE_BUILD_TYPE=RELEASE', prefix_args]
-
-        if is_android:
-            toolchain_path = os.path.join(cmake_project_root_abs_path, 'cmake/android.toolchain.cmake')
-            cmake_line.append('-DCMAKE_TOOLCHAIN_FILE={0}'.format(toolchain_path))
-
-        make_install = build_system_args
-        make_install.append('install')
-
-        build_external_system_args = bs_external.cmd_line()
-
+    def build_snappy(self, cmake_line, make_install):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/snappy.git', abs_dir_path)
             os.chdir(cloned_dir)
 
             os.mkdir('build_cmake_release')
             os.chdir('build_cmake_release')
-            common_cmake_line = list(cmake_line)
+            snappy_cmake_line = list(cmake_line)
             cmake_policy = run_command.CmakePolicy(print_message)
             make_policy = run_command.CommonPolicy(print_message)
-            run_command.run_command_cb(common_cmake_line, cmake_policy)
+            run_command.run_command_cb(snappy_cmake_line, cmake_policy)
             run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_common(self, cmake_line, make_install):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/common.git', abs_dir_path)
             os.chdir(cloned_dir)
@@ -114,35 +85,22 @@ class BuildRequest(object):
             run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
-            raise ex
-
-        try:
-            cloned_dir = utils.git_clone('https://github.com/fastogt/qscintilla.git', abs_dir_path)
-            qsci_src_path = os.path.join(cloned_dir, 'Qt4Qt5')
-            os.chdir(qsci_src_path)
-
-            os.mkdir('build_cmake_release')
-            os.chdir('build_cmake_release')
-            qscintilla_cmake_line = list(cmake_line)
-            cmake_policy = run_command.CmakePolicy(print_message)
-            make_policy = run_command.CommonPolicy(print_message)
-            run_command.run_command_cb(qscintilla_cmake_line, cmake_policy)
-            run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
-        except Exception as ex:
-            os.chdir(pwd)
             raise ex
 
+    def build_openssl(self, prefix_path):
+        abs_dir_path = self.build_dir_path_
         try:
             openssl_default_version = '1.0.2l'
             compiler_flags = utils.CompileInfo([], [])
             url = '{0}openssl-{1}.{2}'.format(OPENSSL_SRC_ROOT, openssl_default_version, ARCH_OPENSSL_EXT)
             utils.build_from_sources(url, compiler_flags, g_script_path, prefix_path, './config')
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_libssh2(self, cmake_line, make_install):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/libssh2.git', abs_dir_path)
             os.chdir(cloned_dir)
@@ -161,12 +119,11 @@ class BuildRequest(object):
             run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
-        if is_android:
-            return
-
+    def build_jsonc(self, cmake_line, make_install):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/json-c.git', abs_dir_path)
             os.chdir(cloned_dir)
@@ -180,9 +137,30 @@ class BuildRequest(object):
             run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_qscintilla(self, cmake_line, make_install):
+        abs_dir_path = self.build_dir_path_
+        try:
+            cloned_dir = utils.git_clone('https://github.com/fastogt/qscintilla.git', abs_dir_path)
+            qsci_src_path = os.path.join(cloned_dir, 'Qt4Qt5')
+            os.chdir(qsci_src_path)
+
+            os.mkdir('build_cmake_release')
+            os.chdir('build_cmake_release')
+            qscintilla_cmake_line = list(cmake_line)
+            cmake_policy = run_command.CmakePolicy(print_message)
+            make_policy = run_command.CommonPolicy(print_message)
+            run_command.run_command_cb(qscintilla_cmake_line, cmake_policy)
+            run_command.run_command_cb(make_install, make_policy)
+            os.chdir(abs_dir_path)
+        except Exception as ex:
+            os.chdir(abs_dir_path)
+            raise ex
+
+    def build_libmemcached(self, prefix_path, make_install):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/libmemcached.git', abs_dir_path)
             os.chdir(cloned_dir)
@@ -196,15 +174,15 @@ class BuildRequest(object):
             configure_policy = run_command.CommonPolicy(print_message)
             run_command.run_command_cb(configure_libmemcached, configure_policy)
 
-            make_install_libmemcached = list(build_external_system_args)
-            make_install_libmemcached.append('install')
             make_policy = run_command.CommonPolicy(print_message)
-            run_command.run_command_cb(make_install_libmemcached, make_policy)
+            run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_unqlite(self, cmake_line, make_install):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/unqlite.git', abs_dir_path)
             os.chdir(cloned_dir)
@@ -218,30 +196,31 @@ class BuildRequest(object):
             run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_lmdb(self, prefix_path):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/lmdb.git', abs_dir_path)
             os.chdir(cloned_dir)
 
             os.chdir('libraries/liblmdb')
-            make_lmdb = list(build_external_system_args)
-            make_lmdb.append('install_static_lib')
-            make_lmdb.append('prefix={0}'.format(prefix_path))
+            make_lmdb = ['make', 'install_static_lib', 'prefix={0}'.format(prefix_path)]  # FIXME
             make_policy = run_command.CommonPolicy(print_message)
             run_command.run_command_cb(make_lmdb, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_leveldb(self, prefix_path):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/leveldb.git', abs_dir_path)
             os.chdir(cloned_dir)
 
-            make_leveldb = list(build_external_system_args)
-            make_leveldb.append('static')
+            make_leveldb = ['make', 'static']  # FIXME
             make_policy = run_command.CommonPolicy(print_message)
             run_command.run_command_cb(make_leveldb, make_policy)
 
@@ -253,9 +232,11 @@ class BuildRequest(object):
             run_command.run_command_cb(copy_leveldb_libs, copy_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_rocksdb(self, cmake_line, make_install):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/rocksdb.git', abs_dir_path)
             os.chdir(cloned_dir)
@@ -271,9 +252,11 @@ class BuildRequest(object):
             run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_upscaledb(self, prefix_path):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/upscaledb.git', abs_dir_path)
             os.chdir(cloned_dir)
@@ -287,15 +270,16 @@ class BuildRequest(object):
             configure_policy = run_command.CommonPolicy(print_message)
             run_command.run_command_cb(configure_upscaledb, configure_policy)
 
-            make_install_upscaledb = list(build_external_system_args)
-            make_install_upscaledb.append('install')
+            make_install_upscaledb = ['make', 'install']  # FIXME
             make_policy = run_command.CommonPolicy(print_message)
             run_command.run_command_cb(make_install_upscaledb, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build_forestdb(self, cmake_line, make_install):
+        abs_dir_path = self.build_dir_path_
         try:
             cloned_dir = utils.git_clone('https://github.com/fastogt/forestdb.git', abs_dir_path)
             os.chdir(cloned_dir)
@@ -309,9 +293,48 @@ class BuildRequest(object):
             run_command.run_command_cb(make_install, make_policy)
             os.chdir(abs_dir_path)
         except Exception as ex:
-            os.chdir(pwd)
+            os.chdir(abs_dir_path)
             raise ex
 
+    def build(self, bs, prefix_path):
+        cmake_project_root_abs_path = '..'
+        if not os.path.exists(cmake_project_root_abs_path):
+            raise utils.BuildError('invalid cmake_project_root_path: %s' % cmake_project_root_abs_path)
+
+        if not bs:
+            bs = system_info.SUPPORTED_BUILD_SYSTEMS[0]
+
+        if not prefix_path:
+            prefix_path = self.platform_.arch().default_install_prefix_path()
+
+        generator = bs.cmake_generator_arg()
+        build_system_args = bs.cmd_line()
+        # bs_name = bs.name()
+
+        # project static options
+        prefix_args = '-DCMAKE_INSTALL_PREFIX={0}'.format(prefix_path)
+        cmake_line = ['cmake', cmake_project_root_abs_path, generator, '-DCMAKE_BUILD_TYPE=RELEASE', prefix_args]
+
+        make_install = build_system_args
+        make_install.append('install')
+
+        abs_dir_path = self.build_dir_path_
+
+        self.build_snappy(cmake_line, make_install)
+        self.build_common(cmake_line, make_install)
+        self.build_openssl(prefix_path)
+        self.build_libssh2(cmake_line, make_install)
+        self.build_jsonc(cmake_line, make_install)
+        self.build_qscintilla(cmake_line, make_install)
+
+        # database build
+        self.build_libmemcached(prefix_path, make_install)
+        self.build_unqlite(cmake_line, make_install)
+        self.build_lmdb(prefix_path)
+        self.build_leveldb(prefix_path)
+        self.build_rocksdb(cmake_line, make_install)
+        self.build_upscaledb(prefix_path)
+        self.build_forestdb(cmake_line, make_install)
 
 if __name__ == "__main__":
     argc = len(sys.argv)
@@ -333,15 +356,9 @@ if __name__ == "__main__":
         bs = None
 
     if argc > 4:
-        bs_external_str = sys.argv[4]
-        bs_external = system_info.get_supported_build_system_by_name(bs_external_str)
-    else:
-        bs_external = None
-
-    if argc > 5:
         prefix_path = sys.argv[5]
     else:
         prefix_path = None
 
-    request = BuildRequest(platform_str, arch_bit_str)
-    request.build('build_' + platform_str + '_env', bs, bs_external, prefix_path)
+    request = BuildRequest(platform_str, arch_bit_str, 'build_' + platform_str + '_env')
+    request.build(bs, prefix_path)
