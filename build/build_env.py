8b5a5110849400cd001c8c5aaad88c278ce8a24c
topilski <atopilski@gmail.com>
2018-02-10 04:47:35 +0300

    1.16.2 rc

--- build/build_env.py
+++ build/build_env.py
@@ -257,6 +257,7 @@ class BuildRequest(object):
             common_cmake_line.append('-DWITH_LZ4=ON')
             common_cmake_line.append('-DROCKSDB_INSTALL_ON_WINDOWS=ON')
             common_cmake_line.append('-DWITH_TOOLS=OFF')
+            common_cmake_line.append('-DWITH_GFLAGS=OFF')
             cmake_policy = run_command.CmakePolicy(print_message)
             make_policy = run_command.CommonPolicy(print_message)
             run_command.run_command_cb(common_cmake_line, cmake_policy)
