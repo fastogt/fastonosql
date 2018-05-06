11ca2eebe7b1efb74f05cf93efe0c0b79082167c
topilski <atopilski@gmail.com>
2018-05-06 15:41:45 +0300

    Static link hiredis

--- build/build_env.py
+++ build/build_env.py
@@ -175,7 +175,8 @@ class BuildRequest(object):
             cloned_dir = utils.git_clone('https://github.com/fastogt/hiredis.git', abs_dir_path)
             os.chdir(cloned_dir)
 
-            make_horedis = ['make', 'LIBSSH2_ENABLED=ON', 'PREFIX={0}'.format(prefix_path), 'install']  # FIXME
+            make_horedis = ['make', 'LIBSSH2_ENABLED=ON', 'OPENSSL_ROOT_DIR={0}'.format(prefix_path)
+                            'PREFIX={0}'.format(prefix_path), 'install']
             make_policy = run_command.CommonPolicy(print_message)
             run_command.run_command_cb(make_horedis, make_policy)
             os.chdir(abs_dir_path)
